4c8feae8dd231592f451c71f6e5a30bd2b45d692
Marco Bettiolo <marco@bettiolo.it>
2013-12-08 03:06:24 +0000

    Splitted Track

--- src/app/audio/filter.js
+++ src/app/audio/filter.js
@@ -20,46 +20,6 @@ function init() {
 	djConsole.rightTrack.play();
 }
 
-function Track(context) {
-	this._audio = new Audio();
-	this._source = context.createMediaElementSource(this._audio);
-	this._gain = context.createGainNode();
-	this._source.connect(this._gain);
-}
-
-Track.prototype.connect = function (audioNode) {
-	this._gain.disconnect();
-	this._gain.connect(audioNode);
-};
-
-Track.prototype.setSrc = function (src) {
-	this._audio.src = src;
-};
-
-Track.prototype.play = function () {
-	this._audio.play();
-};
-
-Track.prototype.pause = function () {
-	this._audio.pause();
-};
-
-Track.prototype.toggle = function () {
-	if (this.isPaused()) {
-		this.play();
-	} else {
-		this.pause();
-	}
-};
-
-Track.prototype.isPaused = function () {
-	return this._audio.paused;
-};
-
-Track.prototype.setGain = function (fraction) {
-	this._gain.gain.value = fraction;
-};
-
 function Ui(djConsole) {
 	this._masterVolumeRange = document.getElementById('master-volume-range');
 	this._crossfadeRange = document.getElementById('crossfade-range');
