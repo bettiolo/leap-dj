708b494851db2ae60061b68a4a919569c62bc092
Marco Bettiolo <marco@bettiolo.it>
2013-12-17 00:35:30 +0000

    Added music streaming from soundcloud

--- src/app/soundcloud-client.js
+++ src/app/soundcloud-client.js
@@ -1,3 +1,31 @@
-/**
- * Created by mak on 16/12/2013.
- */
+function SoundCloud() {
+	this.clientId = 'b07647ddc8d151245fe17e491af8299d';
+	SC.initialize({
+		client_id: this.clientId,
+		redirect_uri: ''
+	});
+}
+
+SoundCloud.prototype.loadTracks = function (tracksLoadedCallBack) {
+	var self = this;
+	SC.get('/playlists/9165886', {}, function(playlist){
+		var soundCloudTracks = playlist.tracks;
+		var trackCount = soundCloudTracks.length;
+		self.tracks = [];
+		for (var i = 0; i < trackCount; i++) {
+			var soundCloudTrack = soundCloudTracks[i];
+			if (soundCloudTrack.streamable) {
+				self.tracks.push(self.convertTrack(soundCloudTrack));
+			}
+		}
+		tracksLoadedCallBack();
+	});
+}
+
+SoundCloud.prototype.convertTrack = function (soundCloudTrack) {
+	var trackInfo = new TrackInfo();
+	trackInfo.url = soundCloudTrack.stream_url + '?client_id=' + this.clientId;
+	trackInfo.title = soundCloudTrack.title;
+	trackInfo.durationMs = soundCloudTrack.duration;
+	return trackInfo;
+}
